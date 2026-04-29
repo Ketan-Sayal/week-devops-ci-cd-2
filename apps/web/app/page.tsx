5e88ccc6a5ea8445f642395e1a5fa70004010f36
username <156992063+Ketandetrfyd13rd3ydf7h@users.noreply.github.com>
2026-04-29 20:43:19 +0530

    added all

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,14 @@
+import { prisma as prismaClient } from "@repo/db/client";
+
+export default async function Home() {
+  const users = await prismaClient.user.findMany();
+  return (
+    <div>
+      {JSON.stringify(users)}
+    </div>
+  );
+}
+
+// export const revalidate = 60 // revalidate every 60 seconds
+// or
+export const dynamic = 'force-dynamic'
